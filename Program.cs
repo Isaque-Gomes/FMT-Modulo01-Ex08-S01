a21cf2c569226698f6ce46adbb6c609b4464a7d0
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-09 12:15:35 -0300

    [M1S01] Ex 8 - Fibonacci
    [Estrutura de Repetição, Arrays (também é possível criar apenas com Repetição)]
    Crie um programa que calcule a sequência de Fibonacci, A sequência de fibonacci, é uma sequência iniciada por 0 e 1, e cada número subsequente é calculado pela soma dos dois anteriores, no caso 0 +1 = 1, o próximo e 1 +1 = 2, seguindo 1 +2 = 3, e assim por diante. chegando a fórmula Fn = F(n- 1) + (n-2).
    
    Os primeiros números da sequência são: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377…
    
    Seu programa deve exibir os primeiros 20 números da sequência de Fibonacci.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Fibonacci (sequência iniciada por 0 e 1 e cada número subsequente é calculado pela soma dos dois anteriores)
+int num1 = 0;
+int num2 = 1;
+int seqFibo;
+
+Console.WriteLine("Nesse programa, vamos calcular os primeiros 20 números da sequência de Fibonacci!");
+
+Console.Write(1 + ",");
+for(int i = 0; i <= 20; i++){
+    seqFibo = num1 + num2;
+    Console.Write(seqFibo + ",");
+    num1 = num2;
+    num2 = seqFibo;
+}
